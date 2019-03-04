603549bec4b4d5314101eb11b004ab5076bdfe05
a643298 <a643298@MC0ZAB0C.ww930.my-it-solutions.net>
2019-03-04 21:23:34 +0200

    1

---
+++ src/test2/test3.java
@@ -0,0 +1,42 @@
+package test2;
+
+
+import java.util.Scanner;
+
+public class test3 {
+
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("O expresie: ");
+	String s = scanner.nextLine();
+	
+	
+	if (s.contains(" "))
+	{
+		System.out.println("No spaces allowed!");
+	} else if ( s.contains("*") && s.contains("+"))
+	{
+		String[] operanzi = s.split("(?=[+*])|(?<=[+*])");
+		int rezultat = Integer.parseInt(operanzi[0]);
+		for(int i=1;i<operanzi.length;i+=2)
+		{	
+			   String op = operanzi[i];
+			   int numar = Integer.parseInt(operanzi[i+1]);
+			   System.out.println(numar);
+			   
+			   if(op=="*")
+			   {
+				  rezultat*=numar; 
+			   }
+			   
+		    }
+		System.out.println(rezultat);
+		}
+		
+		else
+	{
+		System.out.println("No valid operator found!");
+	}	
+}
+}
