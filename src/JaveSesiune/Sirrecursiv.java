fb625e80fd2e3e33f3f8cf67af9b617f3c84019a
a643298 <a643298@MC0ZAB0C.ww930.my-it-solutions.net>
2019-03-05 19:11:52 +0200

    1

---
+++ src/JaveSesiune/Sirrecursiv.java
@@ -0,0 +1,19 @@
+package JaveSesiune;
+
+public class Sirrecursiv {
+
+	public static void permutari(String prefix, String s) {
+		if (s.length() == 1) {
+			System.out.println(prefix + s);
+		}
+
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			permutari("" + prefix + ch, s.substring(0, i) + s.substring(i + 1, s.length()));
+		}
+	}
+
+	public static void main(String[] args) {
+		permutari("", "abc");
+	}
+}
