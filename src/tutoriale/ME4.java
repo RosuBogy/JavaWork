22d4d533df051da1526c7d2b3f83ccb409ab92c5
a643298 <a643298@MC0ZAB0C.ww930.my-it-solutions.net>
2019-03-05 20:17:44 +0200

    1

---
+++ src/tutoriale/ME4.java
@@ -0,0 +1,25 @@
+package tutoriale;
+
+public class ME4 {
+	public static int eval(String s) {
+
+		if (s.contains("+")) {
+			int index = s.indexOf("+");
+			String left = s.substring(0, index);
+			String right = s.substring(index + 1);
+			return eval(left) + eval(right);
+		} else if (s.contains("*")) {
+			int index = s.indexOf("*");
+			String left = s.substring(0, index);
+			String right = s.substring(index + 1);
+			return eval(left) * eval(right);
+
+		}
+
+		return Integer.parseInt(s);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(eval("5*2+3*7+10"));
+	}
+}
