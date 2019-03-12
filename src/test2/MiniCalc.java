1d76619e523a361791b0dd8b1dc6765a0326a3db
a643298 <a643298@MC0ZAB0C.ww930.my-it-solutions.net>
2019-03-12 18:32:24 +0200

    1

---
+++ src/test2/MiniCalc.java
@@ -0,0 +1,57 @@
+package test2;
+
+import java.util.Scanner;
+
+public class MiniCalc {
+	private double curValue = 0;
+
+	public double doOperation(String operation) {
+		if (!(operation.substring(1).matches("^[0-9]*"))) {
+			return curValue;
+		}
+
+		char operatie = operation.charAt(0);
+		double operand = Double.parseDouble(operation.substring(1));
+
+		switch (operatie) {
+		case '+':
+			curValue += operand;
+			break;
+		case '-':
+			curValue -= operand;
+			break;
+		case '*':
+			curValue *= operand;
+			break;
+		case '/':
+			curValue /= operand;
+			break;
+		case '=':
+			curValue = operand;
+			break;
+		default:
+			return curValue;
+		}
+
+		return curValue;
+
+	}
+
+	public static void main(String[] args) {
+
+		String op = null;
+
+		Scanner scanner = new Scanner(System.in);
+		MiniCalc calc = new MiniCalc();
+
+		while (true) {
+			System.out.print(">");
+			op = scanner.nextLine();
+			if (op == "x") {
+				break;
+			}
+			System.out.println(calc.doOperation(op));
+		}
+
+	}
+}
