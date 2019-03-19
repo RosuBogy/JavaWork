cceb81d58ea7bb5404fcb5f7d528dee8c962f8c5
a643298 <a643298@MC0ZAB0C.ww930.my-it-solutions.net>
2019-03-19 18:32:57 +0200

    1

---
+++ src/testcurs/Armonica.java
@@ -0,0 +1,17 @@
+package testcurs;
+
+public class Armonica {
+
+	public static double harmonicAvg(int a, int b) {
+		if (a + b == 0) {
+			return 0;
+		}
+
+		return (2 * a * b) / (a + b);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(harmonicAvg(10, 20));
+	}
+
+}
