56e8026c9c8bc960f74ff1ba1f473b1f82a98d97
a643298 <a643298@MC0ZAB0C.ww930.my-it-solutions.net>
2019-05-23 18:39:09 +0300

    1

--- src/testcurs/Combinari.java
+++ src/testcurs/Combinari.java
@@ -1,22 +1,18 @@
 package testcurs;
 
 public class Combinari {
-	public static void permutari(String prefix, String s) {
-		if (s.length() == 1) {
-			System.out.println(prefix + s);
-		}
-
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			permutari("" + ch, s.substring(0, i) + s.substring(i + 1, s.length()));
-		}
-	}
 
 	public static void combinations(String s, int n) {
 		if (s.length() == n) {
 			System.out.println(s);
 		}
-		permutari("", s);
+		for (int i = 0; i < s.length(); i++) {
+			if (n > 0) {
+				combinations(s.substring(i), n - 1);
+			}
+
+		}
+
 	}
 
 	public static void main(String[] args) {
